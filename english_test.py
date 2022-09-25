e2386ad00fe68ae226179bf631f0d756c91030af
Anshler <huynhminhtriet2002@gmail.com>
2022-09-26 00:19:18 +0700

    update

--- english_test.py
+++ english_test.py
@@ -49,7 +49,7 @@ tfidf_vectorizer=TfidfVectorizer(stop_words='english', max_df=0.7)
 tfidf_train=tfidf_vectorizer.fit_transform(x_train)
 tfidf_test=tfidf_vectorizer.transform(x_test)
 #lưu vectorizer
-dump(tfidf_vectorizer,'tfidf.joblib')
+#dump(tfidf_vectorizer,'tfidf.joblib')
 
 #tạo model
 model = PassiveAggressiveClassifier(max_iter=100)
@@ -64,4 +64,4 @@ score=accuracy_score(y_test,y_pred)
 print(f'Accuracy: {round(score*100,2)}%')
 
 #lưu model
-dump(model,'model.joblib')
+#dump(model,'model.joblib')
